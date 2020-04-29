14ea85e41f46765616a8582ce1da07062bfa1334
Candy <candy.espulgar@gmail.com>
2020-04-29 09:48:08 +0200

    Checkpoint

--- scripts/array_splitter.py
+++ scripts/array_splitter.py
@@ -5,7 +5,7 @@ import numpy
 def split(original_array, split_count = 2):
 
     results = numpy.array_split(numpy.array(original_array), split_count)
-    
+
     return results
 
 
--- scripts/type_1/thread.py
+++ scripts/type_1/thread.py
@@ -1,9 +1,11 @@
 from type_1 import selection_sort
 
 
-def sort(unsorted_array):
+def sort(unsorted_array, thread_name = "Thread"):
     sorted_array = selection_sort.sort_array(unsorted_array)
 
+    print()
+    print(thread_name)
     print("Unsorted Array : " + str(unsorted_array))
     print("Selection Sort : " + str(sorted_array))
 
