02ad2760521ced28e0f4fb2d8e03352b518ef076
Candy <candy.espulgar@gmail.com>
2020-04-29 09:15:22 +0200

    Array Splitter Update

--- scripts/type_1/thread_handler.py
+++ scripts/type_1/thread_handler.py
@@ -1,13 +1,17 @@
+
+import array_splitter
 from type_1 import thread
 import threading
-
 
 def run(unsorted_array):
 
     threads = []
-    threads_num = 2
+    thread_count = 2
 
-    for i in range(threads_num):
+    print(unsorted_array)
+    array_splitter.split(unsorted_array, thread_count)
+
+    for i in range(thread_count):
         t = threading.Thread(target = thread.sort, args = (unsorted_array,))
         threads.append(t)
         t.start()
